7ab8e201ecf2362926dd0f68ca06cbf6ae8d25b0
Michael-Boethas <68383329+Michael-Boethas@users.noreply.github.com>
2024-08-12 23:55:29 +0200

    Ajout des routes update et delete

--- backend/app.js
+++ backend/app.js
@@ -8,12 +8,13 @@ const app = express();
 app.use(express.json()); // Pour traiter les requêtes avec des données au format JSON
 
 mongoose
-  .connect(MONGODB_CONNECTION_STRING)   // Connection à MongoDB
+  .connect(MONGODB_CONNECTION_STRING) // Connexion à MongoDB
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch((err) => console.error("Connexion à MongoDB échouée :", err));
 
 
-///////////////////// Spécifications de l'API ////////////////////////////////////////////////////////
+
+///////////////////// Middleware ////////////////////////////////////////////////////////
 
 const getBooks = (req, res, next) => {
   Book.find() // Méthode Mongoose qui retourne tous les Books
@@ -22,7 +23,7 @@ const getBooks = (req, res, next) => {
 };
 
 const getBookByID = (req, res, next) => {
-  Book.findOne({ _id: req.params.id }) // Récupère un objet par son _id MongoDB
+  Book.findOne({ _id: req.params.id }) // Récupère un Book par son _id MongoDB en le comparant à celui en paramètre dans l'URL de la requête
     .then((book) => res.status(200).json(book))
     .catch((err) => res.status(404).json({ err }));
 };
@@ -30,7 +31,7 @@ const getBookByID = (req, res, next) => {
 const postBook = (req, res, next) => {
   delete req.body._id; // Retire un éventuel champs "_id" qui entrerait en conflit avec celui fournit par MongoDB
   const book = new Book({
-    ...req.body, // L'opérateur spread permet ici de recréer la liste des champs du body puis d'en ajouter
+    ...req.body, // L'opérateur spread permet ici de recréer la liste des champs du body
   });
   book
     .save() // Méthode Mongoose pour enregistrer le nouvel objet dans MongoDB
@@ -38,8 +39,22 @@ const postBook = (req, res, next) => {
     .catch((err) => res.status(400).json({ err }));
 };
 
+const updateBook = (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body }) // Mise à jour du Book avec le nouveau req.body
+      .then(() => res.status(200).json({ message: "Modification du livre bien enregistrée !" }))
+      .catch((err) => res.status(400).json({ err }));
+};
+
+const deleteBook = (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id }) // Suppression du Book avec l'id en paramètre dans l'URL de la requête
+    .then(() => res.status(200).json({ message: "Le livre a bien été supprimé !"}))
+    .catch(err => res.status(400).json({ err}));
+};
+
 app.get(API_ROUTES.BOOKS, getBooks);
 app.get(API_ROUTES.BOOK_BY_ID, getBookByID);
 app.post(API_ROUTES.BOOKS, postBook);
+app.put(API_ROUTES.BOOK_BY_ID, updateBook);
+app.delete(API_ROUTES.BOOK_BY_ID, deleteBook);
 
 export default app;
--- backend/models/book.js
+++ backend/models/book.js
@@ -11,7 +11,7 @@ const bookSchema = new mongoose.Schema({
     {
       userId: { type: String, required: true },
       grade: { type: Number, required: true },
-    }
+    },
   ],
   averageRating: { type: Number, required: true },
 });
