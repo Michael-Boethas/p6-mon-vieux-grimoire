5785241ee677bfdc50340f9c22309ff78e4ec217
Michael-Boethas <68383329+Michael-Boethas@users.noreply.github.com>
2024-12-24 13:57:27 +0100

    Ajout d'un lien vers la collection Postman

--- backend/middleware/setHeaders.js
+++ backend/middleware/setHeaders.js
@@ -4,8 +4,10 @@ import helmet from 'helmet'
 // Headers cross-origin avec cors et sécurité générale avec helmet
 
 const corsOptions = {
-  origin: `http://${process.env.ALLOWED_ORIGIN_DOMAIN || 'localhost'}:${process.env.ALLOWED_ORIGIN_PORT || 3000}`,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  origin: [
+    `http://${process.env.ALLOWED_ORIGIN_DOMAIN || 'localhost'}:${process.env.ALLOWED_ORIGIN_PORT || 3000}`,
+    'https://run.pstmn.io' // Autorisation pour le bouton "Run in Postman"
+  ],  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: [
     'Origin',
     'X-Requested-With',
@@ -15,6 +17,15 @@ const corsOptions = {
   ]
 }
 
-const setHeaders = [cors(corsOptions), helmet()]
+const helmetOptions = {
+  contentSecurityPolicy: {
+    directives: {
+      imgSrc: ["'self'", 'https://run.pstmn.io'], // Autorisation pour le bouton "Run in Postman"
+      scriptSrc: ["'self'", 'https://run.pstmn.io'] // Autorisation pour le bouton "Run in Postman"
+    }
+  }
+};
+
+const setHeaders = [cors(corsOptions), helmet(helmetOptions)]
 
 export default setHeaders
